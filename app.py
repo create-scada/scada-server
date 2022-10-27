3250fd5a4b8d69edba6bb9050d94f03eaa4a3575
daniel-p741 <postdl@mail.irsc.edu>
2022-10-27 17:44:41 -0400

    This will update app.py to fix issue #14

--- app.py
+++ app.py
@@ -12,42 +12,46 @@ env.mode = mode
 
 
 
-if mode == 'production':
-    env.config['database'] = pw.SqliteDatabase('lumen-production.db')
-    env.config['historical_database'] = MongoClient()[
-        'lumen-production']['sensors']
 
 from api.locations import locations
 from api.historian import historian
 from api.misc import misc
 from model import Tables
 
-app = Flask(__name__)
-app.register_blueprint(locations)
-app.register_blueprint(historian)
-app.register_blueprint(misc)
 
 
-@app.before_request
-def before_req():
-    if 'ConnId' in request.headers and request.headers['ConnId'] != '':
+
+def create_app():
+    app = Flask(__name__)
+    if mode == 'production':
+        env.config['database'] = pw.SqliteDatabase('lumen-production.db')
+        env.config['historical_database'] = MongoClient()[
+            'lumen-production']['sensors']
+
+    @app.before_request
+    def before_req():
+     if 'ConnId' in request.headers and request.headers['ConnId'] != '':
         conn_id = request.headers['ConnId']
         if conn_id in connections.DbConnections:
             env.config['database'] = connections.DbConnections[conn_id]['database']
             env.config['database'].bind(Tables)
             env.config['historical_database'] = connections.DbConnections[conn_id]['historical_database']
 
-
-@app.after_request
-def after_request(response):
-    response.headers.add('Allow', 'OPTIONS, GET, HEAD')
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', '*')
-    response.headers.add('Access-Control-Allow-Methods',
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Allow', 'OPTIONS, GET, HEAD')
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Headers', '*')
+        response.headers.add('Access-Control-Allow-Methods',
                          'DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT')
-    response.headers.add('Vary', 'Origin')
-    return response
+        response.headers.add('Vary', 'Origin')
+        return response
+    return app
 
+app = create_app()
+app.register_blueprint(locations)
+app.register_blueprint(historian)
+app.register_blueprint(misc)
 
 if __name__ == '__main__':
     try:
